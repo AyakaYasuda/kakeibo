87f2e09ad670fa961b3886caab8afe3c885356ab
Ayaka Yasuda <ayakayasuda23@gmail.com>
2022-05-11 20:23:43 -0700

    updated spendingController by adding getSpendingByUserId/updateSpending/deleteSpending

--- server/routes/spending-routes.js
+++ server/routes/spending-routes.js
@@ -1,12 +1,12 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
 
-const HttpError = require("../models/http-error");
 const spendingController = require("../controllers/spending-controller");
 
 const router = Router();
 
 router.get("/:sid", spendingController.getSpendingById);
+router.get("/user/:uid", spendingController.getSpendingByUserId);
 router.post(
   "/",
   [
@@ -16,5 +16,15 @@ router.post(
   ],
   spendingController.createSpending
 );
+router.patch(
+  "/:sid",
+  [
+    check("category").notEmpty(),
+    check("title").notEmpty(),
+    check("amount").notEmpty(),
+  ],
+  spendingController.updateSpending
+);
+router.delete("/:sid", spendingController.deleteSpending);
 
 module.exports = router;
