80fdb680bafe392c904787447f41c8399b5bc612
AyakaYasuda <ayakayasuda23@gmail.com>
2022-08-23 02:52:30 -0700

    changed styles for buttons in NoSpending

--- client/src/components/spending/NoSpending.js
+++ client/src/components/spending/NoSpending.js
@@ -10,12 +10,14 @@ const NoSpending = () => {
       <img src={Shocked} alt="shocked" />
       <div className={classes.message}>
         <h2>No spending recorded yet this month!</h2>
-        <Button size="small" to="/spending/new">
-          Record Spending
-        </Button>
-        <Button size="small" to="/spending">
-          Spending History
-        </Button>
+        <div className={classes.buttons}>
+          <Button size="small" to="/spending/new">
+            Record Spending
+          </Button>
+          <Button size="small" to="/spending">
+            Spending History
+          </Button>
+        </div>
       </div>
     </div>
   );
